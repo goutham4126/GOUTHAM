e2a53d3a80c9e103194a578571f0bbede3c24eb2
Goutham <goutham4126@gmail.com>
2026-02-26 21:12:37 +0530

    Frontend

---
+++ frontend/src/app/components/claims-officer/claims-officer-dashboard/claims-officer-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-claims-officer-dashboard',
+  imports: [],
+  templateUrl: './claims-officer-dashboard.html',
+  styleUrl: './claims-officer-dashboard.css',
+})
+export class ClaimsOfficerDashboard {
+
+}
