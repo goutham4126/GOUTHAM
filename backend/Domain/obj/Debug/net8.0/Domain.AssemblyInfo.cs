cc94e2cac8765354d9249d7abeeea19ce3915d1f
Goutham <goutham4126@gmail.com>
2026-02-27 14:12:45 +0530

    backend

--- backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098807288c80e95497d796b181ceba63bf691ab8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1128d2620db0d2a5163824fbe2fb2ee64de6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098807288c80e95497d796b181ceba63bf691ab8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1128d2620db0d2a5163824fbe2fb2ee64de6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
